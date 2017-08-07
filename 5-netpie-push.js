99f034863eb5e2a0b3c9ec6780bbb72c41ed6519
System Administrator <i@chavee.com>
2017-08-07 14:57:17 +0700

    add title and payload checking

--- 5-netpie-push.js
+++ 5-netpie-push.js
@@ -22,19 +22,21 @@ module.exports = function(RED) {
 	    	var payload = config.payloadType=='str'?config.payload:(msg[config.payload]||'');
 	    	var titlearg = title?'?title='+title:'';
 
-			rest.post('https://api.netpie.io/push/owner/'+config.target+titlearg, {
-				headers: {"Content-Type": "text/plain"},
-				username: auth[0],
-				password: auth[1],
-				data: payload.toString()
-			}).on('complete', function(data) {
-				var msg = {
-	        		topic : "&status",
-	        		payload : jsonparse(data),
-			        raw_payload: data
-	        	};
-	            node.send(msg);
-			});
+	    	if (payload) {
+				rest.post('https://api.netpie.io/push/owner/'+config.target+titlearg, {
+					headers: {"Content-Type": "text/plain"},
+					username: auth[0],
+					password: auth[1],
+					data: payload.toString()
+				}).on('complete', function(data) {
+					var msg = {
+		        		topic : "&status",
+		        		payload : jsonparse(data),
+				        raw_payload: data
+		        	};
+		            node.send(msg);
+				});
+			}
         });
     }
     RED.nodes.registerType("push",NetpiePushNode);
